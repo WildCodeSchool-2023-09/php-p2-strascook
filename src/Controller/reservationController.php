db2b7e486ce2dafc4f6b191022faf9575b738eef
Stephdu33 <stephanievivien@hotmail.com>
2023-11-09 10:19:29 +0100

    reservation sans css modifié 091123 version2

--- src/Controller/reservationController.php
+++ src/Controller/reservationController.php
@@ -6,6 +6,6 @@ class ReservationController extends AbstractController
 {
     public function index()
     {
-        return $this->twig->render('Reservation/index.html.twig');
+        return $this->twig->render('Reservation/reservation.html.twig');
     }
 }
