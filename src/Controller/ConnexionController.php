1d2472466e295484e1b9dfb078f089f6e6c09d5a
Laura WOLFF <laura.wolff@live.fr>
2023-11-09 15:34:32 +0100

    création formulaire connexion

---
+++ src/Controller/ConnexionController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Controller;
+
+use App\Controller\AbstractController;
+
+class ConnexionController extends AbstractController
+{
+    public function index() 
+    {
+        return $this->twig->render('Connexion/connexion.html.twig');
+    }
+}
