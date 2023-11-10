a72b325ac9fc0d056e247f2a52c6f7a71ec8d816
Lionel D <lionel.deri@gmail.com>
2023-11-10 13:55:45 +0100

    mise en place de la gestion des CARDS via le MVC

--- database.sql
+++ database.sql
@@ -1,63 +1,371 @@
--- phpMyAdmin SQL Dump
--- version 4.5.4.1deb2ubuntu2
--- http://www.phpmyadmin.net
---
--- Client :  localhost
--- Généré le :  Jeu 26 Octobre 2017 à 13:53
--- Version du serveur :  5.7.19-0ubuntu0.16.04.1
--- Version de PHP :  7.0.22-0ubuntu0.16.04.1
+CREATE DATABASE
+    IF NOT EXISTS `stras_cook`
+    /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */
+    /*!80016 DEFAULT ENCRYPTION='N' */
+;
 
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET time_zone = "+00:00";
+USE `stras_cook`;
 
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
+-- MySQL dump 10.13  Distrib 8.0.16, for Win64 (x86_64)
 
 --
--- Base de données :  `simple-mvc`
+
+-- Host: 127.0.0.1    Database: stras_cook
+
+-- ------------------------------------------------------
+
+-- Server version	8.0.34
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */
+
+;
+
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */
+
+;
+
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */
+
+;
+
+SET NAMES utf8 ;
+
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */
+
+;
+
+/*!40103 SET TIME_ZONE='+00:00' */
+
+;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */
+
+;
+
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */
+
+;
+
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */
+
+;
+
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */
+
+;
+
 --
 
--- --------------------------------------------------------
+-- Table structure for table `client`
 
 --
--- Structure de la table `item`
---
 
-CREATE TABLE `item` (
-  `id` int(11) UNSIGNED NOT NULL,
-  `title` varchar(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+DROP TABLE IF EXISTS `client`;
 
---
--- Contenu de la table `item`
---
+/*!40101 SET @saved_cs_client     = @@character_set_client */
 
-INSERT INTO `item` (`id`, `title`) VALUES
-(1, 'Stuff'),
-(2, 'Doodads');
+;
 
---
--- Index pour les tables exportées
---
+SET character_set_client = utf8mb4 ;
 
---
--- Index pour la table `item`
---
-ALTER TABLE `item`
-  ADD PRIMARY KEY (`id`);
+CREATE TABLE
+    `client` (
+        `id` int NOT NULL AUTO_INCREMENT,
+        `nom` varchar(50) DEFAULT NULL,
+        `prenom` varchar(50) DEFAULT NULL,
+        `tel` varchar(15) DEFAULT NULL,
+        `adresse` varchar(200) DEFAULT NULL,
+        `email` varchar(100) DEFAULT NULL,
+        `identifiant` varchar(25) DEFAULT NULL,
+        `mot_de_passe` varchar(20) DEFAULT NULL,
+        PRIMARY KEY (`id`)
+    ) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci;
 
---
--- AUTO_INCREMENT pour les tables exportées
---
+/*!40101 SET character_set_client = @saved_cs_client */
+
+;
 
 --
--- AUTO_INCREMENT pour la table `item`
+
+-- Dumping data for table `client`
+
 --
-ALTER TABLE `item`
-  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+
+LOCK TABLES `client` WRITE;
+
+/*!40000 ALTER TABLE `client` DISABLE KEYS */
+
+;
+
+/*!40000 ALTER TABLE `client` ENABLE KEYS */
+
+;
+
+UNLOCK TABLES;
+
+--
+
+-- Table structure for table `menus`
+
+--
+
+DROP TABLE IF EXISTS `menus`;
+
+/*!40101 SET @saved_cs_client     = @@character_set_client */
+
+;
+
+SET character_set_client = utf8mb4 ;
+
+CREATE TABLE
+    `menus` (
+        `id` int NOT NULL AUTO_INCREMENT,
+        `nom` varchar(80) DEFAULT NULL,
+        `description` varchar(255) DEFAULT NULL,
+        `photo` varchar(255) DEFAULT NULL,
+        `prix` int DEFAULT NULL,
+        `thematiques_id` int NOT NULL,
+        PRIMARY KEY (`id`, `thematiques_id`),
+        KEY `fk_menus_thematiques1_idx` (`thematiques_id`),
+        CONSTRAINT `fk_menus_thematiques1` FOREIGN KEY (`thematiques_id`) REFERENCES `thematiques` (`id`)
+    ) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci;
+
+/*!40101 SET character_set_client = @saved_cs_client */
+
+;
+
+--
+
+-- Dumping data for table `menus`
+
+--
+
+LOCK TABLES `menus` WRITE;
+
+/*!40000 ALTER TABLE `menus` DISABLE KEYS */
+
+;
+
+/*!40000 ALTER TABLE `menus` ENABLE KEYS */
+
+;
+
+UNLOCK TABLES;
+
+--
+
+-- Table structure for table `produits`
+
+--
+
+DROP TABLE IF EXISTS `produits`;
+
+/*!40101 SET @saved_cs_client     = @@character_set_client */
+
+;
+
+SET character_set_client = utf8mb4 ;
+
+CREATE TABLE
+    `produits` (
+        `id` int NOT NULL AUTO_INCREMENT,
+        `nom` varchar(100) DEFAULT NULL,
+        `type` varchar(255) DEFAULT NULL,
+        `sous_type` varchar(255) DEFAULT NULL,
+        PRIMARY KEY (`id`)
+    ) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci;
+
+/*!40101 SET character_set_client = @saved_cs_client */
+
+;
+
+--
+
+-- Dumping data for table `produits`
+
+--
+
+LOCK TABLES `produits` WRITE;
+
+/*!40000 ALTER TABLE `produits` DISABLE KEYS */
+
+;
+
+/*!40000 ALTER TABLE `produits` ENABLE KEYS */
+
+;
+
+UNLOCK TABLES;
+
+--
+
+-- Table structure for table `produits_menus`
+
+--
+
+DROP TABLE IF EXISTS `produits_menus`;
+
+/*!40101 SET @saved_cs_client     = @@character_set_client */
+
+;
+
+SET character_set_client = utf8mb4 ;
+
+CREATE TABLE
+    `produits_menus` (
+        `produits_id` int NOT NULL,
+        `menus_id` int NOT NULL,
+        PRIMARY KEY (`produits_id`, `menus_id`),
+        KEY `fk_produits_has_menus_menus1_idx` (`menus_id`),
+        KEY `fk_produits_has_menus_produits1_idx` (`produits_id`),
+        CONSTRAINT `fk_produits_has_menus_menus1` FOREIGN KEY (`menus_id`) REFERENCES `menus` (`id`),
+        CONSTRAINT `fk_produits_has_menus_produits1` FOREIGN KEY (`produits_id`) REFERENCES `produits` (`id`)
+    ) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci;
+
+/*!40101 SET character_set_client = @saved_cs_client */
+
+;
+
+--
+
+-- Dumping data for table `produits_menus`
+
+--
+
+LOCK TABLES `produits_menus` WRITE;
+
+/*!40000 ALTER TABLE `produits_menus` DISABLE KEYS */
+
+;
+
+/*!40000 ALTER TABLE `produits_menus` ENABLE KEYS */
+
+;
+
+UNLOCK TABLES;
+
+--
+
+-- Table structure for table `reservation`
+
+--
+
+DROP TABLE IF EXISTS `reservation`;
+
+/*!40101 SET @saved_cs_client     = @@character_set_client */
+
+;
+
+SET character_set_client = utf8mb4 ;
+
+CREATE TABLE
+    `reservation` (
+        `menus_id` int NOT NULL,
+        `client_id` int NOT NULL,
+        `date` date NOT NULL,
+        PRIMARY KEY (`menus_id`, `client_id`),
+        KEY `fk_menus_has_client_client1_idx` (`client_id`),
+        KEY `fk_menus_has_client_menus_idx` (`menus_id`),
+        CONSTRAINT `fk_menus_has_client_client1` FOREIGN KEY (`client_id`) REFERENCES `client` (`id`),
+        CONSTRAINT `fk_menus_has_client_menus` FOREIGN KEY (`menus_id`) REFERENCES `menus` (`id`)
+    ) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci;
+
+/*!40101 SET character_set_client = @saved_cs_client */
+
+;
+
+--
+
+-- Dumping data for table `reservation`
+
+--
+
+LOCK TABLES `reservation` WRITE;
+
+/*!40000 ALTER TABLE `reservation` DISABLE KEYS */
+
+;
+
+/*!40000 ALTER TABLE `reservation` ENABLE KEYS */
+
+;
+
+UNLOCK TABLES;
+
+--
+
+-- Table structure for table `thematiques`
+
+--
+
+DROP TABLE IF EXISTS `thematiques`;
+
+/*!40101 SET @saved_cs_client     = @@character_set_client */
+
+;
+
+SET character_set_client = utf8mb4 ;
+
+CREATE TABLE
+    `thematiques` (
+        `id` int NOT NULL AUTO_INCREMENT,
+        `nom` varchar(100) DEFAULT NULL,
+        `description` varchar(255) DEFAULT NULL,
+        PRIMARY KEY (`id`)
+    ) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci;
+
+/*!40101 SET character_set_client = @saved_cs_client */
+
+;
+
+--
+
+-- Dumping data for table `thematiques`
+
+--
+
+LOCK TABLES `thematiques` WRITE;
+
+/*!40000 ALTER TABLE `thematiques` DISABLE KEYS */
+
+;
+
+/*!40000 ALTER TABLE `thematiques` ENABLE KEYS */
+
+;
+
+UNLOCK TABLES;
+
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */
+
+;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */
+
+;
+
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */
+
+;
+
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */
+
+;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */
+
+;
+
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */
+
+;
+
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */
+
+;
+
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */
+
+;
+
+-- Dump completed on 2023-10-31 10:04:56
--- src/Controller/MenusController.php
+++ src/Controller/MenusController.php
@@ -1,13 +1,20 @@
 <?php
 
+// namespace actuel
 namespace App\Controller;
 
+// On va avoir besoin du AbstractController pour hériter de ses methodes.
 use App\Controller\AbstractController;
+// On va avoir besoin du Model MenuManager pour instancier l'objet.
+use App\Model\MenusManager;
 
 class MenusController extends AbstractController
 {
     public function index()
     {
-        return $this->twig->render('menus/menus.html.twig');
+        $menuManager = new MenusManager(); // On crée une instance de MenuManager.
+        $menus = $menuManager->selectAll(); // Tous les elements du selectAll se retrouvent dans $menu.
+        // On passe en parametre $menu (je ne sait pas pourquoi...)
+        return $this->twig->render('menus/menus.html.twig', ['menus' => $menus]);
     }
 }
